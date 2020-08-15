193d93f36273d1d56fc65eecc6cce85bb730a127
sjin9696 <sjin9696@gmail.com>
2020-08-15 14:03:47 +0900

    Update xmlDataManager.cs
    >> 데이터매니저에 XML형태로 저장하도록 작업

--- 실행파일_KB Storage/KB 스토리지 작업중 200814 최신 UI 3차 진행중(화면+기능)  - 승원, 종훈, 진영/WindowsFormsApp1/xmlDataManager.cs
+++ 실행파일_KB Storage/KB 스토리지 작업중 200814 최신 UI 3차 진행중(화면+기능)  - 승원, 종훈, 진영/WindowsFormsApp1/xmlDataManager.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection.Emit;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace xmlDataManager
 {
+    static class tag
+    {
+        public const string HEAD = "<HEAD>";
+    }
+   
     class xmlDataManager
     {
+        
+
         public static List<Product> Products = new List<Product>();
         public static List<UserInfo> userInfos = new List<UserInfo>();
 
@@ -18,6 +26,18 @@ namespace xmlDataManager
             //string fileName = @".ProductsList.xml";
             File.WriteAllText(@"./ProductsList.xml", contents);
         }
+        //기능 만들기
+        //XML로 출력하기.
+        public static void Save_Products()
+        {
+            const string HEAD = "<Product>";
+            const string NEWLINE = "\n";
+            
+            string productOutput = "";
+            productOutput += HEAD + NEWLINE;
 
+        }
     }
+
+    
 }
