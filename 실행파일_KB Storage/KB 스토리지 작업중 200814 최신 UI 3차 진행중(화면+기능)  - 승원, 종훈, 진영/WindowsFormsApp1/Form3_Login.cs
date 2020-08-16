2b29b30cab9313bf0844dfcfe7d21eff4848cc77
sjin9696 <sjin9696@gmail.com>
2020-08-16 19:36:37 +0900

    로그인창- 로그인체크 함수화 및 열쇠버튼에도 적용

--- 실행파일_KB Storage/KB 스토리지 작업중 200814 최신 UI 3차 진행중(화면+기능)  - 승원, 종훈, 진영/WindowsFormsApp1/Form3_Login.cs
+++ 실행파일_KB Storage/KB 스토리지 작업중 200814 최신 UI 3차 진행중(화면+기능)  - 승원, 종훈, 진영/WindowsFormsApp1/Form3_Login.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace xmlDataManager
@@ -19,13 +12,32 @@ namespace xmlDataManager
 
         private void button_login_Click(object sender, EventArgs e)
         {
+            login_check();
+        }
+
+        private void button_admin_login_Click(object sender, EventArgs e)
+        {
+            login_check();
+        }
+
+        private void textBox_login_id_Click(object sender, EventArgs e)
+        {
+            textBox_login_id.Text = "";
+        }
+
+        private void textBox_login_pw_Click(object sender, EventArgs e)
+        {
+            textBox_login_pw.Text = "";
+        }
+        
+        private void login_check()
+        {
             xmlDataManager.Load_File();
             Form1_Factory_screen Factory_screen = new Form1_Factory_screen();
             Form2_Receiving_screen Receiving_screen = new Form2_Receiving_screen();
-
             string check = userDataManager.Login_Check(textBox_login_id.Text, textBox_login_pw.Text);
-            Console.WriteLine("check : "+check);
-            switch(check)
+            Console.WriteLine("check : " + check);
+            switch (check)
             {
                 case "관리자":
                     Factory_screen.ShowDialog();
@@ -39,25 +51,5 @@ namespace xmlDataManager
                     break;
             }
         }
-
-        private void button_admin_login_Click(object sender, EventArgs e)
-        {
-            xmlDataManager.Load_File();
-            userDataManager.Load_User_Data();
-            Form1_Factory_screen form1 = new Form1_Factory_screen();
-            form1.ShowDialog();
-        }
-
-        private void textBox_login_id_Click(object sender, EventArgs e)
-        {
-            textBox_login_id.Text = "";
-        }
-
-        private void textBox_login_pw_Click(object sender, EventArgs e)
-        {
-            textBox_login_pw.Text = "";
-        }
-
-
     }
 }
