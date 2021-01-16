76d88ff0054fbd9a0b4ecf30343aacdb4037916b
sjin9696 <sjin9696@gmail.com>
2021-01-17 02:05:35 +0900

    product class object modified

--- 실행파일_KB Storage/KB 스토리지 작업중 200814 최신 UI 3차 진행중(화면+기능)  - 승원, 종훈, 진영/WindowsFormsApp1/Products.cs
+++ 실행파일_KB Storage/KB 스토리지 작업중 200814 최신 UI 3차 진행중(화면+기능)  - 승원, 종훈, 진영/WindowsFormsApp1/Products.cs
@@ -3,30 +3,28 @@
     class Product
     {
         //코드
-        public string product_code_number { get; set; }
+        public string code { get; set; }
         //품명
-        public string product_name { get; set; }
+        public string name { get; set; }
         //분류
-        public string categorize_name { get; set; }
+        public string categorize { get; set; }
         //규격
-        public string type_name { get; set; }
+        public string type { get; set; }
         //작업자
-        public string worker_name { get; set; }
+        public string buy_company { get; set; }
         //납품처
-        public string customer_name { get; set; }
+        public string buy_date { get; set; }
         //납품일자
-        public string delivery_dates { get; set; }
+        public string buy_money { get; set; }
         //금액
-        public string money_number { get; set; }
+        public string storage_location { get; set; }
         //보관위치
-        public string storage_name { get; set; }
+        public string storage_quantity { get; set; }
         //재고수량
-        public string storage_inventoy_number { get; set; }
+        public string sales_money { get; set; }
+        //판매수량
+        public string sales_dates { get; set; }
         //판매수량
         public string sales_company { get; set; }
-        //판매수량
-        public string sales_money_number { get; set; }
-        //판매일자
-        public string sales_dates { get; set; }
     }
 }
