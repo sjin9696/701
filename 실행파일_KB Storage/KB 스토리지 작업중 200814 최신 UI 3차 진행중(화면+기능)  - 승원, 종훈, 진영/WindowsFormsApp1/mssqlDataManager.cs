09cd9f79e729f641189fff8503a4c5fe065fb0c5
sjin9696 <sjin9696@gmail.com>
2021-01-16 16:39:17 +0900

    mssqlDataManager code modified

--- 실행파일_KB Storage/KB 스토리지 작업중 200814 최신 UI 3차 진행중(화면+기능)  - 승원, 종훈, 진영/WindowsFormsApp1/mssqlDataManager.cs
+++ 실행파일_KB Storage/KB 스토리지 작업중 200814 최신 UI 3차 진행중(화면+기능)  - 승원, 종훈, 진영/WindowsFormsApp1/mssqlDataManager.cs
@@ -16,30 +16,15 @@ namespace Storagy
             conn.Open();
             sqlcmd.Connection = conn;  //쿼리 날릴곳은 conn, 즉 아까 연결한 DB
             sqlcmd.CommandText = "select * from dbo.storage_db";
-         
-            //SqlDataReader sd = sqlcmd.ExecuteReader();
 
             SqlDataAdapter da = new SqlDataAdapter(sqlcmd);
             DataSet ds = new DataSet();
             da.Fill(ds);
-            
-            /*//쿼리 맵핑
-            foreach (SqlDataReader item in sd)
-            {
-                Console.WriteLine(item.ToString());
-            }*/
-            //while (sd.Read())
-            //{
-            //    Console.WriteLine(sd[0].ToString());
-            //    Console.WriteLine(sd.Read());
-            //}
             
             sqlcmd.Connection.Close();
             conn.Close();
 
             return ds;
         }
-        //조회 기능. 
-        //매순간 DB 연결후 종료 하기. 방식. 
     }
 }
