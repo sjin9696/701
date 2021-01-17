ab6d01d5cb23af68fc3075bfca701d0f2f5ee88d
sjin9696 <sjin9696@gmail.com>
2021-01-17 22:09:14 +0900

    form6_search update

--- 실행파일_KB Storage/KB 스토리지 작업중 200814 최신 UI 3차 진행중(화면+기능)  - 승원, 종훈, 진영/WindowsFormsApp1/Form6_search_screen.Designer.cs
+++ 실행파일_KB Storage/KB 스토리지 작업중 200814 최신 UI 3차 진행중(화면+기능)  - 승원, 종훈, 진영/WindowsFormsApp1/Form6_search_screen.Designer.cs
@@ -31,20 +31,22 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form6_search_screen));
             this.label11 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox1_code = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox2_name = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox3_categorize = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button_search = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.productBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.productBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4_type = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // label11
@@ -58,65 +60,65 @@
             this.label11.Text = "검  색";
             this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox1
+            // textBox1_code
             // 
-            this.textBox1.AllowDrop = true;
-            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(124, 130);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(122, 29);
-            this.textBox1.TabIndex = 69;
+            this.textBox1_code.AllowDrop = true;
+            this.textBox1_code.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1_code.Location = new System.Drawing.Point(124, 128);
+            this.textBox1_code.Multiline = true;
+            this.textBox1_code.Name = "textBox1_code";
+            this.textBox1_code.Size = new System.Drawing.Size(122, 29);
+            this.textBox1_code.TabIndex = 69;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(76, 133);
+            this.label3.Location = new System.Drawing.Point(76, 131);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(42, 22);
             this.label3.TabIndex = 68;
             this.label3.Text = "코드";
             // 
-            // textBox2
+            // textBox2_name
             // 
-            this.textBox2.AllowDrop = true;
-            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(381, 130);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(122, 29);
-            this.textBox2.TabIndex = 71;
+            this.textBox2_name.AllowDrop = true;
+            this.textBox2_name.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2_name.Location = new System.Drawing.Point(312, 128);
+            this.textBox2_name.Multiline = true;
+            this.textBox2_name.Name = "textBox2_name";
+            this.textBox2_name.Size = new System.Drawing.Size(122, 29);
+            this.textBox2_name.TabIndex = 71;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(333, 133);
+            this.label4.Location = new System.Drawing.Point(264, 131);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(42, 22);
             this.label4.TabIndex = 70;
             this.label4.Text = "품명";
             // 
-            // textBox3
+            // textBox3_categorize
             // 
-            this.textBox3.AllowDrop = true;
-            this.textBox3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(662, 130);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(122, 29);
-            this.textBox3.TabIndex = 73;
+            this.textBox3_categorize.AllowDrop = true;
+            this.textBox3_categorize.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3_categorize.Location = new System.Drawing.Point(528, 128);
+            this.textBox3_categorize.Multiline = true;
+            this.textBox3_categorize.Name = "textBox3_categorize";
+            this.textBox3_categorize.Size = new System.Drawing.Size(122, 29);
+            this.textBox3_categorize.TabIndex = 73;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(598, 133);
+            this.label1.Location = new System.Drawing.Point(480, 131);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 22);
+            this.label1.Size = new System.Drawing.Size(42, 22);
             this.label1.TabIndex = 72;
-            this.label1.Text = "납품처";
+            this.label1.Text = "분류";
             // 
             // button_search
             // 
@@ -125,7 +127,7 @@
             this.button_search.FlatAppearance.BorderSize = 0;
             this.button_search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_search.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_search.Location = new System.Drawing.Point(834, 123);
+            this.button_search.Location = new System.Drawing.Point(876, 123);
             this.button_search.Name = "button_search";
             this.button_search.Size = new System.Drawing.Size(56, 36);
             this.button_search.TabIndex = 83;
@@ -141,20 +143,42 @@
             this.button2.TabIndex = 83;
             this.button2.Text = "선택";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_insert_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(55, 171);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 23;
+            this.dataGridView.Size = new System.Drawing.Size(951, 524);
+            this.dataGridView.TabIndex = 85;
+            this.dataGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            this.dataGridView.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellEnter);
             // 
             // productBindingSource1
             // 
             this.productBindingSource1.DataSource = typeof(Storagy.xmlDataManager);
             // 
-            // dataGridView1
+            // label2
             // 
-            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(55, 171);
-            this.dataGridView.Name = "dataGridView1";
-            this.dataGridView.RowTemplate.Height = 23;
-            this.dataGridView.Size = new System.Drawing.Size(951, 524);
-            this.dataGridView.TabIndex = 85;
-            this.dataGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(668, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 22);
+            this.label2.TabIndex = 72;
+            this.label2.Text = "규격";
+            // 
+            // textBox4_type
+            // 
+            this.textBox4_type.AllowDrop = true;
+            this.textBox4_type.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4_type.Location = new System.Drawing.Point(732, 128);
+            this.textBox4_type.Multiline = true;
+            this.textBox4_type.Name = "textBox4_type";
+            this.textBox4_type.Size = new System.Drawing.Size(122, 29);
+            this.textBox4_type.TabIndex = 73;
             // 
             // Form6_search_screen
             // 
@@ -164,11 +188,13 @@
             this.ClientSize = new System.Drawing.Size(1066, 770);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button_search);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4_type);
+            this.Controls.Add(this.textBox3_categorize);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox2_name);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox1_code);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.dataGridView);
@@ -176,8 +202,8 @@
             this.Text = "창고관리프로그램";
             this.Load += new System.EventHandler(this.Form6_search_screen_Load);
             ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,16 +211,18 @@
 
         #endregion
         private System.Windows.Forms.Label label11;
-        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.TextBox textBox1_code;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox2_name;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox3_categorize;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button_search;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.BindingSource productBindingSource;
         private System.Windows.Forms.BindingSource productBindingSource1;
         private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4_type;
     }
 }
--- 실행파일_KB Storage/KB 스토리지 작업중 200814 최신 UI 3차 진행중(화면+기능)  - 승원, 종훈, 진영/WindowsFormsApp1/Form6_search_screen.cs
+++ 실행파일_KB Storage/KB 스토리지 작업중 200814 최신 UI 3차 진행중(화면+기능)  - 승원, 종훈, 진영/WindowsFormsApp1/Form6_search_screen.cs
@@ -9,9 +9,6 @@ namespace Storagy
         public Form6_search_screen()
         {
             InitializeComponent();
-            //dataGridView1.DataSource = new mssqlDataManager().ConnectDB().Tables[0];
-            //dataGridView_all_incentory.DataSource = xmlDataManager.ProductsList;
-            xmlDataManager.Load_File();
         }
         
         private void Form6_search_screen_Load(object sender, EventArgs e)
@@ -20,27 +17,39 @@ namespace Storagy
             dataGridView.DataSource = new mssqlDataManager().SelectDB("select " +
                 "code, [name], categorize, type from dbo.storage_db;").Tables[0];
         }
+
+        private void dataGridView_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            Product.temporary_Product.code = dataGridView.Rows[e.RowIndex].Cells[0].Value.ToString();
+            Product.temporary_Product.name = dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+            Product.temporary_Product.categorize = dataGridView.Rows[e.RowIndex].Cells[2].Value.ToString();
+            Product.temporary_Product.type = dataGridView.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+
         private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            Product.temporary_Product.code          = dataGridView.Rows[e.RowIndex].Cells[0].Value.ToString();
-            Product.temporary_Product.name          = dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
-            Product.temporary_Product.categorize    = dataGridView.Rows[e.RowIndex].Cells[2].Value.ToString();
-            Product.temporary_Product.type          = dataGridView.Rows[e.RowIndex].Cells[3].Value.ToString();
-
             MessageBox.Show("데이터가 불러들여졌습니다.");
             Close();
         }
 
         private void button_search_Click(object sender, EventArgs e)
         {
-            xmlDataManager.Load_File();
-            List<Product> products_list = new List<Product>();
-            products_list = xmlDataManager.ProductsList.FindAll(item =>
-                (item.code == textBox1.Text) ||
-                (item.name == textBox2.Text) ||
-                (item.buy_date == textBox3.Text));
+            string select = "SELECT code, [name], categorize, type FROM dbo.storage_db ";
+            string where =  "WHERE name ='"         + textBox2_name.Text +
+                            "' OR categorize = '"   + textBox3_categorize.Text +
+                            "' OR type = '"         + textBox4_type.Text + "' ";
+            string query = "IF NOT EXISTS(" + select + where + ")BEGIN " + select +
+                "END " + "ELSE BEGIN " + select + where + "END ";
 
-            //dataGridView_all_incentory.DataSource = products_list;
+            dataGridView.DataSource = "";
+            dataGridView.DataSource = new mssqlDataManager().SelectDB(query).Tables[0];
         }
+
+        private void button_insert_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("데이터가 불러들여졌습니다.");
+            Close();
+        }
+        
     }
 }
